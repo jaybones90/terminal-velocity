5f2e803644e0784b3af97784e7dfd5b48efbcfda
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-06-16 11:58:51 -0700

    link program with firebase and seed test data to make sure connection is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import { masterFirebaseConfig } from './api-keys';
-// import { AngularFireModule } from 'angularfire2';
-// import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  projectId: masterFirebaseConfig.projectId,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -20,6 +28,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     FormsModule,
     HttpModule,
     routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/team-member.model.ts
@@ -0,0 +1,3 @@
+export class TeamMember {
+  constructor(public name: string, public nickName: string, public age: number, public sex: string, public height: string, public weight: number, public homeTown: string, public jumps: number, public sponsers: string, public bio: string ) {}
+}
