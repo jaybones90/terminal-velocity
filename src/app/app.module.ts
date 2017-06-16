401f914b3591aef11e608529d6f4983c986f1966
Jason Ainsworth git config --global user.email jasoncruze@gmail.comgit config --global user.name Jason <J-Bones@Jasons-MacBook.local>
2017-06-16 11:31:09 -0700

    add in additonal files to handle routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+// import { masterFirebaseConfig } from './api-keys';
+// import { AngularFireModule } from 'angularfire2';
+// import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
